abc691752e2976df2cff3b671e011c7a7623143a
bluebird <nttruclol@gmail.com>
2019-11-18 14:15:18 +0700

    truc

--- app/src/main/java/com/trucandphat/tnpblog/LoginActivity.java
+++ app/src/main/java/com/trucandphat/tnpblog/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.trucandphat.tnpblog;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -11,16 +14,21 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 
 public class LoginActivity extends AppCompatActivity {
 
     private SignInButton googleSigninBtn;
     public static GoogleSignInClient mGoogleSignInClient;
+    private static int GG_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setEvent();
     }
     protected void setEvent() {
         this.googleSigninBtn = findViewById(R.id.google_signin_button);
@@ -31,5 +39,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+
 
 }
--- app/src/main/java/com/trucandphat/tnpblog/ui/blog/BlogFragment.java
+++ app/src/main/java/com/trucandphat/tnpblog/ui/blog/BlogFragment.java
@@ -2,6 +2,7 @@ package com.trucandphat.tnpblog.ui.blog;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,27 +10,64 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.trucandphat.tnpblog.R;
 
 public class BlogFragment extends Fragment {
 
-    private BlogViewModel blogViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        blogViewModel =
-                ViewModelProviders.of(this).get(BlogViewModel.class);
         View root = inflater.inflate(R.layout.fragment_blogs, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        blogViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        BottomNavigationView bottomNav = root.findViewById(R.id.top_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListner);
+        getFragmentManager().beginTransaction().replace(R.id.blog_fragment_container, new BlogStudyFragment()).commit();
         return root;
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    FragmentManager t = getFragmentManager();
+                    String fragTag="";
+                    Fragment selectedFragment = null;
+                    if(t.findFragmentByTag("Study") != null){
+                        //if the other fragment is visible, hide it.
+                        t.beginTransaction().hide(t.findFragmentByTag("Study")).commit();
+                    }
+                    if(t.findFragmentByTag("Love") != null){
+                        //if the other fragment is visible, hide it.
+                        t.beginTransaction().hide(t.findFragmentByTag("Love")).commit();
+                    }
+                    if(t.findFragmentByTag("Entertainment") != null){
+                        //if the other fragment is visible, hide it.
+                        t.beginTransaction().hide(t.findFragmentByTag("Entertainment")).commit();
+                    }
+
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_study:
+                            selectedFragment = new BlogStudyFragment();
+                            fragTag="Study";
+                            break;
+                        case R.id.nav_love:
+                            selectedFragment = new BlogLoveFragment();
+                            fragTag="Love";
+                            break;
+                        case R.id.nav_entertainment:
+                            selectedFragment = new BlogEntertainmentFragment();
+                            fragTag="Entertainment";
+                            break;
+                    }
+
+                    if(getFragmentManager().findFragmentByTag(fragTag)!=null)
+                        t.beginTransaction().show(getFragmentManager().findFragmentByTag(fragTag)).commit();
+                    else
+                        t.beginTransaction().add(R.id.blog_fragment_container, selectedFragment,fragTag).addToBackStack(null).commit();
+                    return true;
+                }
+            };
 }
