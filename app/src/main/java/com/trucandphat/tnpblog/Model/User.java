dbea3a73454ead98b331be573a5b5fcfd8b9b37c
Phatnguyen27 <43376008+Phatnguyen27@users.noreply.github.com>
2019-11-27 12:30:23 +0700

    change_variable_name_+_ProgressDialog

--- app/src/main/java/com/trucandphat/tnpblog/Model/User.java
+++ app/src/main/java/com/trucandphat/tnpblog/Model/User.java
@@ -5,14 +5,24 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class User implements Serializable {
+    private static User instance = null;
     private String uid;
+    private String avatar;
     private String username;
+    private String email;
     private Date dateOfBirth;
     private Date dateCreated;
     private int blogNumber;
+    private String id;
 
     public User() {
 
+    }
+    public static User getCurrentUser(){
+        if(instance == null){
+            instance = new User();
+        }
+        return instance;
     }
 
     public String getUid() {
@@ -23,12 +33,15 @@ public class User implements Serializable {
         this.uid = uid;
     }
 
-    public User(String uid, String username, Date dateOfBirth, Date dateCreated,int blogNumber) {
+    public User(String uid, String username,String email, Date dateOfBirth, Date dateCreated,int blogNumber) {
         this.uid = uid;
+        this.avatar = "";
         this.username = username;
+        this.email = email;
         this.dateOfBirth = dateOfBirth;
         this.dateCreated = dateCreated;
         this.blogNumber = blogNumber;
+        id = "";
     }
 
     public String getUsername() {
@@ -37,6 +50,14 @@ public class User implements Serializable {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
     }
 
     public Date getDateOfBirth() {
@@ -53,5 +74,21 @@ public class User implements Serializable {
 
     public void setDateCreated(Date dateCreated) {
         this.dateCreated = dateCreated;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
--- app/src/main/java/com/trucandphat/tnpblog/ui/information/InformationFragment.java
+++ app/src/main/java/com/trucandphat/tnpblog/ui/information/InformationFragment.java
@@ -1,11 +1,26 @@
 package com.trucandphat.tnpblog.ui.information;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -13,41 +28,183 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.trucandphat.tnpblog.LoginActivity;
 import com.trucandphat.tnpblog.Model.User;
 import com.trucandphat.tnpblog.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.app.Activity.RESULT_OK;
 
 public class InformationFragment extends Fragment {
 
     private View root;
-    public static final String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-
-    private EditText mEditName,mEditDOB,mEditDOC;
+    public  String userId;
+    private Button mbtnLogout;
+    private ImageView profileImage;
+    private EditText mEditName,mEditDOB,mEditDOC, mEditEmail;
+    public static GoogleSignInClient mGoogleSignInClient;
+    private GoogleSignInOptions googleSignInOptions;
+    private static int PICK_IMAGE_REQUEST = 23;
+    private Uri filePath;
+    private Dialog loadingDialog;
+    private ProgressDialog initialDialog;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_information, container, false);
+        initiateDialog();
+        initGoogleSignin();
         setEvent();
         loadInformation();
+        mbtnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                mGoogleSignInClient.signOut();
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+            }
+        });
+        //data
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+            }
+        });
+        initialDialog.dismiss();
         return root;
     }
+
+    public void initiateDialog() {
+        initialDialog = new ProgressDialog(getActivity());
+        initialDialog.setTitle("Loading Information...");
+        initialDialog.setMessage("Please wait...");
+        initialDialog.setCancelable(true);
+        initialDialog.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null ){
+            loadingDialog.show();
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(),filePath);
+                profileImage.setImageBitmap(bitmap);
+                Toast.makeText(getContext(),User.getCurrentUser().getUid()+"complete",Toast.LENGTH_SHORT).show();
+            } catch (IOException e){
+                Toast.makeText(getContext(),"fail",Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+            //Toast.makeText(getContext(),User.getCurrentUser().getUid(),Toast.LENGTH_SHORT).show();
+            final StorageReference reference = storageReference.child("userImage/"+User.getCurrentUser().getUid());
+
+            reference.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Map<String, Object> data = new HashMap<>();
+                            data.put("avatar",uri.toString());
+                            User.getCurrentUser().setAvatar(uri.toString());
+
+                            FirebaseDatabase.getInstance().getReference().child("User")
+                                    .child(User.getCurrentUser().getUid()).updateChildren(data)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    loadingDialog.dismiss();
+                                    //Toast.makeText(getContext(),User.getCurrentUser().getUid()+"'s avatar updated complete!",Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getContext(),"fail_lan_cuoi",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getContext(),"fail_something1",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(),"fail_up_image",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    public void initGoogleSignin() {
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(),googleSignInOptions);
+    }
     public void setEvent() {
+        profileImage = root.findViewById(R.id.imageViewAvatar);
         mEditName = root.findViewById(R.id.edit_name);
         mEditDOB = root.findViewById(R.id.edit_dob);
         mEditDOC = root.findViewById(R.id.edit_doc);
+        mEditEmail = root.findViewById(R.id.edit_email);
+        mbtnLogout = root.findViewById(R.id.btnLogout);
+        //Diaglog
+        loadingDialog = new Dialog(getContext());
+        loadingDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        loadingDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        loadingDialog.setContentView(R.layout.item_loading);
     }
     public void loadInformation() {
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         FirebaseDatabase.getInstance().getReference().child("User").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
                     User user = ds.getValue(User.class);
                     if(user.getUid().equals(userId)) {
+                        User.getCurrentUser().setId(user.getId());
+                        User.getCurrentUser().setUid(user.getUid());
+                        User.getCurrentUser().setUsername(user.getUsername());
+                        User.getCurrentUser().setAvatar(user.getAvatar());
+                        User.getCurrentUser().setDateCreated(user.getDateCreated());
+                        //Toast.makeText(getContext(),user.getUsername(),Toast.LENGTH_SHORT).show();
+                        user.getCurrentUser().setId(ds.getKey());
+                        if(user.getCurrentUser().getAvatar() != null) {
+                            new DownloadImageFromInternet(profileImage)
+                                    .execute(user.getCurrentUser().getAvatar());
+                        }
                         mEditName.setText(user.getUsername());
+                        mEditEmail.setText(user.getEmail());
                         mEditDOB.setText(user.getDateOfBirth().toString());
                         mEditDOC.setText(user.getDateCreated().toString());
                     }
@@ -59,5 +216,33 @@ public class InformationFragment extends Fragment {
 
             }
         });
+    }
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+
+        ImageView imageView;
+
+        public DownloadImageFromInternet(ImageView imageView){
+            this.imageView = imageView;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL = urls[0];
+            Bitmap bimage = null;
+            try {
+                InputStream in = new java.net.URL(imageURL).openStream();
+                bimage = BitmapFactory.decodeStream(in);
+
+            } catch (Exception e) {
+                Log.e("Error Message", e.getMessage());
+                e.printStackTrace();
+            }
+            return bimage;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
     }
 }
